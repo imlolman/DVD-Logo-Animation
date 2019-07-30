374a74d6ba455fd57a9bd160b6b99d500c95abcf
Satyam Gupta <satyamforwork@gmail.com>
2019-07-30 23:10:24 +0530

    Updated Screen Size

--- sketch.js
+++ sketch.js
@@ -7,42 +7,42 @@ let speed = {
   y: 5
 }
 let imgPos = {
-  height: 140,
-  width:200
+  height: 70,
+  width: 100
 }
 
-let r,g,b;
+let r, g, b;
 let dvd_logo;
 
-function preload(){
+function preload() {
   dvd_logo = loadImage("logo.png");
 }
 
-function setup(){
-  createCanvas(800,600)
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight)
   getColorforImage()
 }
 
-function draw(){
+function draw() {
   background(0)
-  tint(r,g,b)
-  image(dvd_logo,pos.x,pos.y,imgPos.width,imgPos.height)
+  tint(r, g, b)
+  image(dvd_logo, pos.x, pos.y, imgPos.width, imgPos.height)
 
-  if(pos.x >= width-imgPos.width || pos.x < 0){
+  if (pos.x >= width - imgPos.width || pos.x < 0) {
     speed.x *= -1
     getColorforImage()
   }
 
-  if(pos.y>= height-imgPos.height || pos.y < 0){
-    speed.y*= -1
+  if (pos.y >= height - imgPos.height || pos.y < 0) {
+    speed.y *= -1
     getColorforImage()
   }
-  
+
   pos.x += speed.x;
   pos.y += speed.y;
 }
 
-function getColorforImage(){
+function getColorforImage() {
   r = random(100, 256)
   g = random(100, 256)
   b = random(100, 256)
